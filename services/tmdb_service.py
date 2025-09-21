b228f1a6dea51549403524e6ade34c8b2fa9422d
Otto <otto@masterbranch.io>
2025-09-21 19:29:21 +0200

    handling pagination

--- services/tmdb_service.py
+++ services/tmdb_service.py
@@ -1,6 +1,5 @@
 import config as c
 import requests
-import json
 
 from http import HTTPStatus
 
@@ -9,14 +8,24 @@ class TMDBService:
     def __init__(self, api_key) -> None:
         self._api_key = api_key
 
-    def build_list_url(self, list_id):
-        return f'{c.tmdb_endpoints["list"]}/{list_id}?api_key={self._api_key}'
+    def build_list_url(self, list_id, page):
+        return f'{c.tmdb_endpoints["list"]}/{list_id}?page={page}api_key={self._api_key}'
+
+    def get_list_by_page(self, list_id, page):
+        r = requests.get(self.build_list_url(list_id, page))
+        r.raise_on_error()
+        result = r.json()
+        
+        return [[i.get('title', i.get('original_name')), i['poster_path']] for i in result['items']], r["total_pages"]
 
     def get_list(self, list_id):
-        result = requests.get(self.build_list_url(list_id))
+        total_list = []
 
-        if result.status_code == HTTPStatus.OK:
-            api_response = json.loads(result.text)
-            return [[i.get('title', i.get('original_name')), i['poster_path']] for i in api_response['items']]
+        first_page_items, last_page = self.get_list_by_page(list_id, 1)
+        total_list.extend(first_page)
 
-        return []
+        for idx in range(2, last_page):
+            actual_page_items, _ = self.get_list_by_page(list_id, idx)
+            total_list.extend(actual_page_items)
+
+        return total_list
